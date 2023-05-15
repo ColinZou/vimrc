9a77abfa30c19e2a677b842d37557dca9a4333ed
Kaiping Zou <kaiping.zou@thundersoft.com>
2023-05-15 15:40:52 +0800

    format -> format{async=true}

--- my_configs.lua
+++ my_configs.lua
@@ -138,7 +138,7 @@ cmp.setup {
 vim.api.nvim_create_autocmd("BufWritePre", {
     pattern = { "*.go" },
     callback = function()
-        vim.lsp.buf.format()
+        vim.lsp.buf.format{ async = true }
     end,
 })
 
