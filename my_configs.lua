f28de4758beefac0a031e76f753947092905e8ea
Colin Zou <zoukaiping@gmail.com>
2023-10-17 20:23:54 +0800

    Use telescope to show document symbols

--- my_configs.lua
+++ my_configs.lua
@@ -394,4 +394,5 @@ vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', telescope_builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', telescope_builtin.help_tags, {})
+vim.keymap.set('n', '<leader>ol', telescope_builtin.lsp_document_symbols, {})
 
